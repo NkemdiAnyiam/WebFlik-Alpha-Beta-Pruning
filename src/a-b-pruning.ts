ba3ecbf5a7d7cf66d6d93b06172c121ee3d873ab
Nkemdi Anyiam <NkemdiAnyiam@gmail.com>
2024-04-05 02:41:02 -0500

    Fix several logic errors in alpha-beta pruning algo

--- src/a-b-pruning.ts
+++ src/a-b-pruning.ts
@@ -6,23 +6,18 @@ class TreeNode {
   constructor(public utility: number, public actions: TreeNode[], public depth: number) {}
   
   isTerminal(): boolean { return this.actions.length === 0; }
-  nextify(next: TreeNode | null): TreeNode { this.next = next; return this; }
+  setNext(next: TreeNode | null): TreeNode { this.next = next; return this; }
 }
-
-type Ret = [
-  utility: number,
-  move: null | TreeNode
-];
 
 export function alphaBetaSearch(root: TreeNode) {
-  const [value, move] = maxValue(root, -Infinity, Infinity);
+  const value = maxValue(root, -Infinity, Infinity);
   console.log(value);
-  return move;
+  return root;
 }
 
-function maxValue(node: TreeNode, alpha: number, beta: number): Ret {
+function maxValue(node: TreeNode, alpha: number, beta: number): number {
   if (node.isTerminal()) {
-    return [node.utility, node.nextify(null)];
+    return node.utility;
   }
   node.alpha = alpha;
   node.beta = beta;
@@ -32,108 +27,85 @@ function maxValue(node: TreeNode, alpha: number, beta: number): Ret {
 
   for (let i = 0; i < node.actions.length; ++i) {
     const action = node.actions[i];
-    const [maybeBetterVal, maybeBetterMove] = minValue(action, node.alpha, node.beta);
+    const maybeBetterVal = minValue(action, node.alpha, node.beta);
     if (maybeBetterVal > bestVal) {
       bestVal = maybeBetterVal;
-      bestMove = maybeBetterMove;
+      bestMove = action;
       if (bestVal >= node.beta) {
-        return [bestVal, node.nextify(bestMove)];
+        [node.utility, node.next] = [bestVal, bestMove];
+        return bestVal;
       }
       node.alpha = Math.max(node.alpha, bestVal);
     }
   };
 
-  return [bestVal, node.nextify(bestMove)];
+  [node.utility, node.next] = [bestVal, bestMove];
+  return bestVal;
 }
 
-function minValue(node: TreeNode, alpha: number, beta: number): Ret {
+function minValue(node: TreeNode, alpha: number, beta: number): number {
   if (node.isTerminal()) {
-    return [node.utility, null];
+    return node.utility;
   }
+
+  node.alpha = alpha;
+  node.beta = beta;
   
   let bestVal = Infinity;
   let bestMove = null;
 
   for (let i = 0; i < node.actions.length; ++i) {
     const action = node.actions[i];
-    const [maybeBetterVal, maybeBetterMove] = maxValue(action, node.alpha, node.beta);
+    const maybeBetterVal = maxValue(action, node.alpha, node.beta);
     if (maybeBetterVal < bestVal) {
       bestVal = maybeBetterVal;
-      bestMove = maybeBetterMove;
-      if (bestVal <= alpha) {
-        return [bestVal, node.nextify(bestMove)];
+      bestMove = action;
+      if (bestVal <= node.alpha) {
+        [node.utility, node.next] = [bestVal, bestMove];
+        return bestVal;
       }
-      beta = Math.min(beta, bestVal);
+      node.beta = Math.min(node.beta, bestVal);
     }
   };
 
-  return [bestVal, node.nextify(bestMove)];
+  [node.utility, node.next] = [bestVal, bestMove];
+  return bestVal;
 }
 
-type BootLeg = {
+type BasicNodeStruct = {
   utility: number;
-  actions?: BootLeg[];
+  actions?: BasicNodeStruct[];
 };
 
-function convert(action: BootLeg, depth = 0): TreeNode {
+export function createNodes(action: BasicNodeStruct, depth = 0): TreeNode {
   const node = new TreeNode(
     action.utility,
-    action.actions?.map((action) => convert(action, depth + 1)) ?? [],
+    action.actions?.map((action) => createNodes(action, depth + 1)) ?? [],
     depth
   );
 
   return node;
 }
 
-const node = convert({
-  utility: 3,
-  actions: [
-    {
-      utility: 3,
-      actions: [
-        {utility: 3},
-        {utility: 12},
-        {utility: 8},
-      ]
-    },
-    {
-      utility: 2,
-      actions: [
-        {utility: 2},
-        {utility: 4},
-        {utility: 6},
-      ]
-    },
-    {
-      utility: 2,
-      actions : [
-        {utility: 14},
-        {utility: 5},
-        {utility: 2},
-      ]
-    }
-  ]
-});
+// const queue = [node];
+// let recentDepth = node.depth;
+// let str = ''
+// while (queue.length > 0) {
+//   const curr = queue.shift()!;
+//   queue.push(...curr.actions);
+//   if (recentDepth !== curr.depth) {
+//     recentDepth = curr.depth;
+//     console.log(str);
+//     str = '';
+//   }
 
-const queue = [node];
-let recentDepth = node.depth;
-let str = ''
-while (queue.length > 0) {
-  const curr = queue.shift()!;
-  queue.push(...curr.actions);
-  if (recentDepth !== curr.depth) {
-    recentDepth = curr.depth;
-    console.log(str);
-    str = '';
-  }
+//   str += `D:${curr.depth}, U:${curr.utility}          `;
+// }
+// console.log(str);
 
-  str += `D:${curr.depth}, U:${curr.utility}          `;
-}
-console.log(str);
-
-const move = alphaBetaSearch(node);
-let thing = move;
-while (thing) {
-  console.log(`D:${thing.depth}, U:${thing.utility}`);
-  thing = thing?.next!;
-}
+// const move = alphaBetaSearch(node);
+// let thing = move;
+// while (thing) {
+//   console.log(`D:${thing.depth}, U:${thing.utility}`);
+//   thing = thing?.next!;
+// }
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,55 @@
 console.log('ALPHA BETA');
-import './a-b-pruning';
+import { createNodes, alphaBetaSearch } from "./a-b-pruning";
+
+const root = createNodes({
+  utility: 3,
+  actions: [
+    {
+      utility: 3,
+      actions: [
+        {utility: 3},
+        {utility: 12},
+        {utility: 8},
+      ]
+    },
+    {
+      utility: 2,
+      actions: [
+        {utility: 2},
+        {utility: 4},
+        {utility: 6},
+      ]
+    },
+    {
+      utility: 2,
+      actions : [
+        {utility: 14},
+        {utility: 5},
+        {utility: 2},
+      ]
+    }
+  ]
+});
+
+const queue = [root];
+let recentDepth = root.depth;
+let str = ''
+while (queue.length > 0) {
+  const curr = queue.shift()!;
+  queue.push(...curr.actions);
+  if (recentDepth !== curr.depth) {
+    recentDepth = curr.depth;
+    console.log(str);
+    str = '';
+  }
+
+  str += `D:${curr.depth}, U:${curr.utility}          `;
+}
+console.log(str);
+
+const move = alphaBetaSearch(root);
+let thing = move;
+while (thing) {
+  console.log(`D:${thing.depth}, U:${thing.utility}`);
+  thing = thing?.next!;
+}
